cd8d6ad04ad4210725d9b2cf7dabb81ac18569a1
VV Bassarab <v.bassarab@yandex.ru>
2022-02-15 12:52:12 +0300

    Debug mode: off

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#czgw^#!@f)c0w8wfq%7fh6d@qrcele#g_^l-$@-m!gp24+*0_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
